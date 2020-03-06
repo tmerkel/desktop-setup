679fc8d300a28a6e8da48f7cd152560fd915b5f4
tmerkel <tmerkel@live.com>
2020-03-05 21:43:26 -0800

    Add comments

--- win-setup-pre.ps1
+++ win-setup-pre.ps1
@@ -1,7 +1,9 @@
 # Enable WSL
+Write-Host "Enabling Windows Subsystem for Linux..." -ForegroundColor Blue
 Enable-WindowsOptionalFeature -NoRestart -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 
 # Enable Virtual Machine Platform
+Write-Host "Enabling Virtual Machine Platform..." -ForegroundColor Blue
 Enable-WindowsOptionalFeature -NoRestart -Online -FeatureName VirtualMachinePlatform
 
 Write-Host "Finished installing pre-requisites.  Please restart." -ForegroundColor Green
