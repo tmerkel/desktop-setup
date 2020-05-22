8ebb60d2fbfb6048161987e557bce939206f5e56
Travis Merkel <tmerkel@live.com>
2020-05-22 05:54:29 -0700

    Add header

--- wsl-setup.bash
+++ wsl-setup.bash
@@ -13,6 +13,7 @@ export AWS_SHARED_CREDENTIALS_FILE='C:\Users\tmerk\.aws\credentials'
 
 alias k=kubectl" >> ~/.bashrc
 
+echo "Installing .NetCore 3.1"
 wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo add-apt-repository universe
@@ -20,3 +21,4 @@ sudo apt-get update
 sudo apt-get install apt-transport-https
 sudo apt-get update
 sudo apt-get install dotnet-sdk-3.1
+
